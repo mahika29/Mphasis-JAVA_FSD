c440c3ac3a49563f464b8feb20065fef693e71ac
MAHIKA HARIKUMAR <128667503+mahika29@users.noreply.github.com>
2026-03-27 13:09:23 +0530

    Add files via upload

---
+++ SpringCoreExp2/src/main/java/com/test/Employee.java
@@ -0,0 +1,45 @@
+package com.test;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Employee {
+	private int id;
+	private String name;
+	private String company;
+	
+	@Autowired
+	private List<Address> adr;
+	
+	public List<Address> getAdr() {
+		return adr;
+	}
+	public void setAdr(List<Address> adr) {
+		this.adr = adr;
+	}
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", company=" + company + "]";
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	
+
+}
