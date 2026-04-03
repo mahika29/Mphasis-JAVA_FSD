bd255a622636ba9c1d5b103d02ce35aa423657c7
MAHIKA HARIKUMAR <128667503+mahika29@users.noreply.github.com>
2026-04-03 12:40:52 +0530

    Add files via upload

---
+++ SpringCloudService3/src/main/java/com/test/controller/Service3Controller.java
@@ -0,0 +1,14 @@
+package com.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/service3")
+public class Service3Controller {
+	@GetMapping("/card")
+	public String cardPayments() {
+		return "spring cloud service3 for CARD payments";
+	}
+}
