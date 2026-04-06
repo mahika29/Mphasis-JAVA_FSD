3de6afe783c3eae2aabdbf3b6efc917da9ba7c09
MAHIKA HARIKUMAR <128667503+mahika29@users.noreply.github.com>
2026-04-06 10:03:52 +0530

    Add files via upload

---
+++ StudentMicroserviceFeignClient/src/main/java/com/test/client/StudentClient.java
@@ -0,0 +1,16 @@
+package com.test.client;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.test.entity.Student;
+
+@FeignClient(name="StudentMicroservices",
+url="http://localhost:2222"
+)
+public interface StudentClient {
+	@GetMapping("/list")
+	public List<Student> getAllStudents();
+}
